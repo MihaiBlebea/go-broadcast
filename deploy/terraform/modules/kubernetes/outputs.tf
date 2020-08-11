a017c3b8337e55985cad61c065f4204fcda3ce46
Mihai Blebea <mihai@getchip.uk>
2020-08-11 18:41:08 +0300

    test

--- deploy/terraform/modules/kubernetes/outputs.tf
+++ deploy/terraform/modules/kubernetes/outputs.tf
@@ -1,7 +1,11 @@
-output "loadbalancer_ip" {
-    value = kubernetes_service.blog_load_balancer.ip
-}
+# output "loadbalancer_ip" {
+#     value = kubernetes_service.blog_load_balancer.ip
+# }
 
-output "loadbalancer_hostname" {
-    value = kubernetes_service.blog_load_balancer.hostname
+# output "loadbalancer_hostname" {
+#     value = kubernetes_service.blog_load_balancer.hostname
+# }
+
+output "loadbalancer_raw" {
+    value = kubernetes_service.blog_load_balancer
 }
