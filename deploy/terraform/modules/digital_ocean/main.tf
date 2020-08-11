d077086bfc6f5e788ecc0006f987c9a60ae5985b
Mihai Blebea <mihai@getchip.uk>
2020-08-11 18:22:00 +0300

    Remove the self create lb

--- deploy/terraform/modules/digital_ocean/main.tf
+++ deploy/terraform/modules/digital_ocean/main.tf
@@ -14,25 +14,25 @@ resource "digitalocean_certificate" "mihaiblebea" {
     domains = [digitalocean_domain.mihaiblebea_com.name]
 }
 
-resource "digitalocean_loadbalancer" "public" {
-    name   = "loadbalancer-1"
-    region = "lon1"
+# resource "digitalocean_loadbalancer" "public" {
+#     name   = "loadbalancer-1"
+#     region = "lon1"
 
-    forwarding_rule {
-        entry_port     = 80
-        entry_protocol = "http"
+#     forwarding_rule {
+#         entry_port     = 80
+#         entry_protocol = "http"
 
-        target_port     = 80
-        target_protocol = "http"
-    }
+#         target_port     = 80
+#         target_protocol = "http"
+#     }
 
-    healthcheck {
-        port     = 22
-        protocol = "tcp"
-    }
+#     healthcheck {
+#         port     = 22
+#         protocol = "tcp"
+#     }
 
-    droplet_ids = [digitalocean_kubernetes_cluster.cluster.node_pool[0].nodes[0].droplet_id]
-}
+#     droplet_ids = [digitalocean_kubernetes_cluster.cluster.node_pool[0].nodes[0].droplet_id]
+# }
 
 resource "digitalocean_kubernetes_cluster" "cluster" {
     name    = "blog-k8-cluster-1"
