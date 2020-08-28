e0fac9c86850dbf5aa4b4e21aade988530aa7f11
Mihai Blebea <mihai@getchip.uk>
2020-08-28 09:00:05 +0100

    Comment the cname records

--- deploy/terraform/modules/digital_ocean_lb/main.tf
+++ deploy/terraform/modules/digital_ocean_lb/main.tf
@@ -31,29 +31,29 @@ resource "digitalocean_record" "aws_domain_verification_record" {
     value    = var.aws_domain_verification_token
 }
 
-resource "digitalocean_record" "aws_domain_key_set_one" {
-    domain   = digitalocean_domain.mihaiblebea_com.name
-    type     = "CNAME"
-    name     = "64aonjzwf22bgfitrab5nbu6v2rqnard._domainkey.${digitalocean_domain.mihaiblebea_com.name}"
-    priority = 10
-    value    = "64aonjzwf22bgfitrab5nbu6v2rqnard.dkim.amazonses.com"
-}
+# resource "digitalocean_record" "aws_domain_key_set_one" {
+#     domain   = digitalocean_domain.mihaiblebea_com.name
+#     type     = "CNAME"
+#     name     = "64aonjzwf22bgfitrab5nbu6v2rqnard._domainkey.${digitalocean_domain.mihaiblebea_com.name}"
+#     priority = 10
+#     value    = "64aonjzwf22bgfitrab5nbu6v2rqnard.dkim.amazonses.com"
+# }
 
-resource "digitalocean_record" "aws_domain_key_set_two" {
-    domain   = digitalocean_domain.mihaiblebea_com.name
-    type     = "CNAME"
-    name     = "xhrue3rk4uzwfuskugzrngyxt3jloi5m._domainkey.${digitalocean_domain.mihaiblebea_com.name}"
-    priority = 10
-    value    = "xhrue3rk4uzwfuskugzrngyxt3jloi5m.dkim.amazonses.com"
-}
+# resource "digitalocean_record" "aws_domain_key_set_two" {
+#     domain   = digitalocean_domain.mihaiblebea_com.name
+#     type     = "CNAME"
+#     name     = "xhrue3rk4uzwfuskugzrngyxt3jloi5m._domainkey.${digitalocean_domain.mihaiblebea_com.name}"
+#     priority = 10
+#     value    = "xhrue3rk4uzwfuskugzrngyxt3jloi5m.dkim.amazonses.com"
+# }
 
-resource "digitalocean_record" "aws_domain_key_set_three" {
-    domain   = digitalocean_domain.mihaiblebea_com.name
-    type     = "CNAME"
-    name     = "i2njichgbwkz2xs2suagqzhckea7bvyd._domainkey.${digitalocean_domain.mihaiblebea_com.name}"
-    priority = 10
-    value    = "i2njichgbwkz2xs2suagqzhckea7bvyd.dkim.amazonses.com"
-}
+# resource "digitalocean_record" "aws_domain_key_set_three" {
+#     domain   = digitalocean_domain.mihaiblebea_com.name
+#     type     = "CNAME"
+#     name     = "i2njichgbwkz2xs2suagqzhckea7bvyd._domainkey.${digitalocean_domain.mihaiblebea_com.name}"
+#     priority = 10
+#     value    = "i2njichgbwkz2xs2suagqzhckea7bvyd.dkim.amazonses.com"
+# }
 
 
 resource "digitalocean_loadbalancer" "public" {
